2159fb1d1c15e6b0cc2e3ee79dbd90a381031728
Matin <matin3enator0@gmail.com>
2022-02-20 12:07:39 +0330

    Create basic controller for Books

---
+++ app/src/main/java/com/openlibrary/app/book/BooksController.java
@@ -0,0 +1,24 @@
+package com.openlibrary.app.book;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/books")
+public class BooksController {
+
+    @GetMapping
+    ResponseEntity<?> list() {
+        // list books
+        return null;
+    }
+
+    @GetMapping(path = "/{id}")
+    ResponseEntity<?> get(@PathVariable String id) {
+        // get a book
+        return null;
+    }
+}
