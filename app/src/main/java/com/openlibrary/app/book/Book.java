58050e365d636ec438626cc062bc96c4a13de994
Matin <matin3enator0@gmail.com>
2022-02-21 16:32:36 +0330

    Creating an empty constructor for Book.java

--- app/src/main/java/com/openlibrary/app/book/Book.java
+++ app/src/main/java/com/openlibrary/app/book/Book.java
@@ -38,6 +38,10 @@ public class Book {
         this.language = language;
     }
 
+    public Book() {
+
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
