236dd0fcfac0305477b5e6f1263ab53dbb1926c1
Matin <matin3enator0@gmail.com>
2022-02-21 16:03:46 +0330

    Create a basic google books api service

---
+++ app/src/main/java/com/openlibrary/app/book/GoogleBooksApi.java
@@ -0,0 +1,55 @@
+package com.openlibrary.app.book;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
+
+import java.util.function.Consumer;
+
+@Service
+public class GoogleBooksApi {
+
+    @Value("${google.apis.books.url}")
+    private String baseUrl;
+
+    @Value("${google.apis.books.key}")
+    private String key;
+
+    public void list(String query,
+                     String filter,
+                     int startIndex,
+                     int maxResult,
+                     String projection,
+                     Consumer<Book> booksSubscriber) {
+        Flux<Book> booksFlux = WebClient.create(baseUrl)
+                .get()
+                .uri(uriBuilder ->
+                        uriBuilder.path("/volumes")
+                                .queryParam("q", query)
+                                .queryParam("filter", filter)
+                                .queryParam("projection", projection)
+                                .queryParam("startIndex", startIndex)
+                                .queryParam("maxResult", maxResult)
+                                .queryParam("key", key)
+                                .build()
+                )
+                .retrieve()
+                .bodyToFlux(Book.class);
+        booksFlux.subscribe(booksSubscriber);
+    }
+
+    public void get(String bookId,
+                    Consumer<Book> bookSubscriber) {
+        Flux<Book> bookFlux = WebClient.create(baseUrl)
+                .get()
+                .uri(uriBuilder ->
+                        uriBuilder.path("/volumes")
+                                .queryParam("key", key)
+                                .build(bookId)
+                )
+                .retrieve()
+                .bodyToFlux(Book.class);
+        bookFlux.subscribe(bookSubscriber);
+    }
+}
