34b53c3a17ed6d52262c2e6b04eae1cc5a2653c8
Matin <matin3enator0@gmail.com>
2022-02-23 14:35:47 +0330

    Using rest template to google volumes list request

--- app/src/main/java/com/openlibrary/app/book/GoogleVolumesApi.java
+++ app/src/main/java/com/openlibrary/app/book/GoogleVolumesApi.java
@@ -1,73 +1,48 @@
 package com.openlibrary.app.book;
 
-import com.openlibrary.app.BadRequestException;
-import com.openlibrary.app.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.lang.NonNull;
 import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Service;
-import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Mono;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.Optional;
-import java.util.function.Consumer;
 
 @Service
 public class GoogleVolumesApi {
 
-    @Value("${google.apis.books.url}")
-    private String baseUrl;
-
     @Value("${google.apis.books.key}")
     private String key;
 
-    public Mono<VolumesCollectionResponse> list(@NonNull String query,
-                                                @Nullable String filter,
-                                                @Nullable int startIndex,
-                                                @Nullable int maxResult,
-                                                @Nullable String projection,
-                                                @NonNull Consumer<VolumesCollectionResponse> volumeConsumer,
-                                                @NonNull Consumer<Throwable> onError) {
-        Mono<VolumesCollectionResponse> booksFlux = WebClient.create(baseUrl)
-                .get()
-                .uri(uriBuilder ->
-                        uriBuilder.path("/volumes")
-                                .queryParam("q", query)
-                                .queryParamIfPresent("filter", Optional.ofNullable(filter))
-                                .queryParamIfPresent("projection", Optional.ofNullable(projection))
-                                .queryParamIfPresent("startIndex", Optional.of(startIndex))
-                                .queryParamIfPresent("maxResult", Optional.of(maxResult))
-                                .queryParam("key", key)
-                                .build()
-                )
-                .retrieve()
-                .onStatus(
-                        httpStatus -> httpStatus == HttpStatus.BAD_REQUEST,
-                        clientResponse -> Mono.error(new BadRequestException())
-                )
-                .bodyToMono(VolumesCollectionResponse.class)
-                .doOnError(onError);
-        return booksFlux;
+    private final RestTemplate restTemplate;
+
+    public GoogleVolumesApi(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
     }
 
-    public void get(@NonNull String volumeId,
-                    @NonNull Consumer<VolumeResponse> volumeSubscriber,
-                    @NonNull Consumer<Throwable> onError) {
-        Mono<VolumeResponse> volumeResponseMono = WebClient.create(baseUrl)
-                .get()
-                .uri(uriBuilder ->
-                        uriBuilder.path("/volumes/{volumeId}")
-                                .queryParam("key", key)
-                                .build(volumeId)
-                )
-                .retrieve()
-                .onStatus(
-                        httpStatus -> httpStatus == HttpStatus.NOT_FOUND,
-                        clientResponse -> Mono.error(new ResourceNotFoundException())
-                )
-                .bodyToMono(VolumeResponse.class)
-                .doOnError(onError);
-        volumeResponseMono.subscribe(volumeSubscriber);
+    public VolumesCollectionResponse list(@NonNull String query,
+                                          @Nullable String filter,
+                                          @Nullable int startIndex,
+                                          @Nullable int maxResult,
+                                          @Nullable String projection) {
+        ResponseEntity<VolumesCollectionResponse> response = restTemplate.getForEntity(
+                UriComponentsBuilder.fromPath("/volumes")
+                        .queryParam("q", query)
+                        .queryParamIfPresent("filter", Optional.ofNullable(filter))
+                        .queryParamIfPresent("projection", Optional.ofNullable(projection))
+                        .queryParamIfPresent("startIndex", Optional.of(startIndex))
+                        .queryParamIfPresent("maxResult", Optional.of(maxResult))
+                        .queryParam("key", key)
+                        .build()
+                        .toUri()
+                        .toString(),
+                VolumesCollectionResponse.class
+        );
+        return response.getBody();
+    }
+
+    public void get(@NonNull String volumeId) {
     }
 }
--- app/src/test/java/com/openlibrary/app/book/GoogleVolumesApiTest.java
+++ app/src/test/java/com/openlibrary/app/book/GoogleVolumesApiTest.java
@@ -1,14 +1,11 @@
 package com.openlibrary.app.book;
 
-import com.openlibrary.app.BadRequestException;
-import com.openlibrary.app.ResourceNotFoundException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.concurrent.CountDownLatch;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class GoogleVolumesApiTest {
@@ -17,64 +14,20 @@ class GoogleVolumesApiTest {
     GoogleVolumesApi googleVolumesApi;
 
     @Test
-    void list() throws InterruptedException {
-        CountDownLatch countDownLatch = new CountDownLatch(2);
-        googleVolumesApi.list(
+    void list() {
+        VolumesCollectionResponse result = googleVolumesApi.list(
                 "hamlet",
                 null,
                 0,
-                20,
-                null,
-                volumesCollectionResponse -> {
-                    assertNotEquals(0, volumesCollectionResponse.getTotalItems());
-                    assertNotNull(volumesCollectionResponse.getItems());
-                    assertNotEquals(0, volumesCollectionResponse.getItems().size());
-                    System.out.println(volumesCollectionResponse);
-                    countDownLatch.countDown();
-                },
-                throwable -> {
-                }
-        );
-        googleVolumesApi.list(
-                "",
-                null,
                 0,
-                20,
-                null,
-                volumesCollectionResponse -> {
-                },
-                throwable -> {
-                    assertInstanceOf(BadRequestException.class, throwable);
-                    countDownLatch.countDown();
-                }
+                null
         );
-        countDownLatch.await();
+        assertNotEquals(0, result.getTotalItems());
+        assertNotNull(result.getItems());
     }
 
     @Test
     void get() throws InterruptedException {
-        CountDownLatch countDownLatch = new CountDownLatch(2);
-        googleVolumesApi.get(
-                "S04tXAPxaikC",
-                volume -> {
-                    assertNotNull(volume);
-                    assertNotNull(volume.getId());
-                    assertNotNull(volume.getVolumeInfo());
-                    assertNotNull(volume.getVolumeInfo().getTitle());
-                    countDownLatch.countDown();
-                },
-                throwable -> {
-                }
-        );
-        googleVolumesApi.get(
-                "zyTC0lF9jgY2",
-                volumeResponse -> {
-                },
-                throwable -> {
-                    assertInstanceOf(ResourceNotFoundException.class, throwable);
-                    countDownLatch.countDown();
-                }
-        );
-        countDownLatch.await();
+
     }
 }
