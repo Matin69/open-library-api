dac54c909049c97a434b21bf9e893b438ce5e3a9
Matin <matin3enator0@gmail.com>
2022-02-23 14:34:58 +0330

    Config a base rest template as bean in Application class

--- app/src/main/java/com/openlibrary/app/AppApplication.java
+++ app/src/main/java/com/openlibrary/app/AppApplication.java
@@ -1,15 +1,31 @@
 package com.openlibrary.app;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.DefaultUriBuilderFactory;
 
 @SpringBootApplication
+@Configuration
 @EnableAsync
 public class AppApplication {
+
+    @Value("${google.apis.books.url}")
+    private String baseUrl;
 
     public static void main(String[] args) {
         SpringApplication.run(AppApplication.class, args);
     }
 
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder
+                .uriTemplateHandler(new DefaultUriBuilderFactory(baseUrl))
+                .build();
+    }
 }
