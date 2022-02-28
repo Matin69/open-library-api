25ad810a9cfa40df597dcf94fa14ac983cca05e3
Matin <matin3enator0@gmail.com>
2022-02-28 11:09:24 +0330

    Implement a function to list books in BookService

--- app/src/main/java/com/openlibrary/app/book/BookService.java
+++ app/src/main/java/com/openlibrary/app/book/BookService.java
@@ -1,45 +1,32 @@
 package com.openlibrary.app.book;
 
 import org.springframework.stereotype.Service;
-import reactor.core.publisher.Mono;
 
-import java.util.HashSet;
 import java.util.Set;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
 
 @Service
 public class BookService {
 
     private final GoogleVolumesApi volumesApi;
 
-    public BookService(GoogleVolumesApi volumesApi) {
+    private final BookConverter bookConverter;
+
+    public BookService(GoogleVolumesApi volumesApi, BookConverter bookConverter) {
         this.volumesApi = volumesApi;
+        this.bookConverter = bookConverter;
     }
 
-    public Mono<VolumesCollectionResponse> list(String query,
-                                                String filter,
-                                                int startIndex,
-                                                int maxResult,
-                                                String projection) {
-        Set<Book> books = new HashSet<>();
-        Mono<VolumesCollectionResponse> listMono = volumesApi.list(
-                query,
-                filter,
-                startIndex,
-                maxResult,
-                projection,
-                volumesCollectionResponse -> {
-                    if (volumesCollectionResponse != null) {
-                        volumesCollectionResponse.getItems()
-                                .forEach(volumeResponse -> books.add(new Book()));
-                    }
-                },
-                throwable -> {
-                }
-        );
-        return listMono;
-    }
-
-    public void get() {
-
+    public CompletableFuture<Set<Book>> list(String query,
+                                             String filter,
+                                             int startIndex,
+                                             int maxResult,
+                                             String projection) {
+        VolumesCollectionResponse response = volumesApi.list(query, filter, startIndex, maxResult, projection);
+        Set<Book> books = response.getItems().stream()
+                .map(bookConverter::toBook)
+                .collect(Collectors.toSet());
+        return CompletableFuture.completedFuture(books);
     }
 }
