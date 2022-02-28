c599ac9edf80df7ef772e4ab5a3933cc03c32358
Matin <matin3enator0@gmail.com>
2022-02-28 16:13:23 +0330

    Create a user entity

---
+++ app/src/main/java/com/openlibrary/app/user/User.java
@@ -0,0 +1,34 @@
+package com.openlibrary.app.user;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Data
+@Entity
+public class User {
+
+    @Id
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private String password;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
