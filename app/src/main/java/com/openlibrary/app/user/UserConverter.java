b776df7e8868aeafdb5b20f3278fdfa82fb1b400
Matin <matin3enator0@gmail.com>
2022-03-01 09:44:55 +0330

    Create a converter for User.java for conversion of it's dtos

---
+++ app/src/main/java/com/openlibrary/app/user/UserConverter.java
@@ -0,0 +1,23 @@
+package com.openlibrary.app.user;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserConverter {
+
+    public UserInfo fromUser(User user) {
+        return new UserInfo(
+                user.getId(),
+                user.getName(),
+                user.getEmail()
+        );
+    }
+
+    public User toUser(UserCreateRequest userCreateRequest) {
+        return new User(
+                userCreateRequest.getName(),
+                userCreateRequest.getEmail(),
+                userCreateRequest.getPassword()
+        );
+    }
+}
