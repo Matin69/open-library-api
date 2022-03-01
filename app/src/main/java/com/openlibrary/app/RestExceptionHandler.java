595922e4c3dc1f07de13c8cf1fecf1374ca3ade9
Matin <matin3enator0@gmail.com>
2022-03-01 10:54:59 +0330

    Handle validation error exception

--- app/src/main/java/com/openlibrary/app/RestExceptionHandler.java
+++ app/src/main/java/com/openlibrary/app/RestExceptionHandler.java
@@ -2,8 +2,13 @@ package com.openlibrary.app;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
 public class RestExceptionHandler {
@@ -16,5 +21,19 @@ public class RestExceptionHandler {
     @ExceptionHandler({ResourceNotFoundException.class})
     public ResponseEntity<Object> handleResourceNotFoundException(RuntimeException exception) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
+    }
+
+    @ExceptionHandler({MethodArgumentNotValidException.class})
+    public ResponseEntity<Map<String, String>> handleValidationFailed(MethodArgumentNotValidException notValidException) {
+        Map<String, String> errors = new HashMap<>();
+        notValidException.getBindingResult()
+                .getAllErrors()
+                .forEach(error -> {
+                    String fieldName = ((FieldError) error).getField();
+                    String errorMessage = error.getDefaultMessage();
+                    errors.put(fieldName, errorMessage);
+                });
+        return ResponseEntity.badRequest()
+                .body(errors);
     }
 }
