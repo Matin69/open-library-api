509d3730817dbc259817fee57342b169ca1544e6
Matin <matin3enator0@gmail.com>
2022-03-05 16:25:10 +0330

    Set DevSecurityConfig as default profile

--- app/src/main/java/com/openlibrary/app/security/DevSecurityConfig.java
+++ app/src/main/java/com/openlibrary/app/security/DevSecurityConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
 @Configuration
-@Profile("dev")
+@Profile({"default", "dev"})
 public class DevSecurityConfig extends SecurityConfig {
 
     @Override
