dc9f398785a0ca63c5b825e6ad4b888cc45a7950
Matin <matin3enator0@gmail.com>
2022-03-05 14:50:47 +0330

    Rename RestExceptionHandler.java to ApplicationErrorHandler

--- app/src/main/java/com/openlibrary/app/ApplicationErrorHandler.java
+++ app/src/main/java/com/openlibrary/app/ApplicationErrorHandler.java
@@ -11,7 +11,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @ControllerAdvice
-public class RestExceptionHandler {
+public class ApplicationErrorHandler {
 
     @ExceptionHandler({BadRequestException.class})
     public ResponseEntity<Object> handleBadRequestException(RuntimeException runtimeException) {
