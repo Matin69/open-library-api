ead8825cf545ec32de11ace33ef527d7641de489
Matin <matin3enator0@gmail.com>
2022-03-05 14:48:21 +0330

    Make SecurityConfig abstract

--- app/src/main/java/com/openlibrary/app/security/SecurityConfig.java
+++ app/src/main/java/com/openlibrary/app/security/SecurityConfig.java
@@ -2,5 +2,5 @@ package com.openlibrary.app.security;
 
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public abstract class SecurityConfig extends WebSecurityConfigurerAdapter {
 }
