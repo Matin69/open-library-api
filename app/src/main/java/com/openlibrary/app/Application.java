a0caa2f928035ef473bcca3aae5ee1b93e03f54c
Matin <matin3enator0@gmail.com>
2022-03-21 16:31:23 +0330

    Add @EnableConfigurationProperties to Application.java

--- app/src/main/java/com/openlibrary/app/Application.java
+++ app/src/main/java/com/openlibrary/app/Application.java
@@ -1,8 +1,10 @@
 package com.openlibrary.app;
 
+import com.openlibrary.app.security.GoogleAuthProperties;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,6 +12,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.DefaultUriBuilderFactory;
 
+@EnableConfigurationProperties(GoogleAuthProperties.class)
 @SpringBootApplication
 @Configuration
 @EnableAsync
