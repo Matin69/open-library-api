d0f3f6b28c39def1b306177c8da460e46cedb492
Matin <matin3enator0@gmail.com>
2022-03-27 22:14:07 +0430

    Fix map of different token request response json objects name to it's pojo

--- app/src/main/java/com/openlibrary/app/security/TokenRequestResponse.java
+++ app/src/main/java/com/openlibrary/app/security/TokenRequestResponse.java
@@ -1,17 +1,22 @@
 package com.openlibrary.app.security;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class TokenRequestResponse {
 
+    @JsonProperty("access_token")
     private String accessToken;
 
+    @JsonProperty("token_type")
     private String tokenType;
 
     private String scope;
 
+    @JsonProperty("expires_in")
     private int expiresIn;
 
+    @JsonProperty("refresh_token")
     private String refreshToken;
 }
