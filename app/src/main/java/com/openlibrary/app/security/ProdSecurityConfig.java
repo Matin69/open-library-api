c2710f44da797eb0493888ad4fcfb2c0afc3054e
Matin <matin3enator0@gmail.com>
2022-03-27 11:21:28 +0430

    Remove auth principals from default auth manager

--- app/src/main/java/com/openlibrary/app/security/ProdSecurityConfig.java
+++ app/src/main/java/com/openlibrary/app/security/ProdSecurityConfig.java
@@ -1,30 +1,14 @@
 package com.openlibrary.app.security;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 @Profile("production")
 @Configuration
 public class ProdSecurityConfig extends SecurityConfig {
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder authManagerBuilder) throws Exception {
-        authManagerBuilder.inMemoryAuthentication()
-                .withUser("Admin")
-                .password(passwordEncoder().encode("admin"))
-                .roles("ADMIN")
-                .and()
-                .withUser("User")
-                .password(passwordEncoder().encode("user"))
-                .roles("USER");
-    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -36,10 +20,5 @@ public class ProdSecurityConfig extends SecurityConfig {
                 .csrf().disable()
                 .cors().disable()
                 .formLogin();
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
     }
 }
