318744efcf6181554155cde7e2f02da636d99d08
Matin <matin3enator0@gmail.com>
2022-03-28 16:16:47 +0430

    Remove all methods and add a new method to get user private shelves in ShelvesController

--- app/src/main/java/com/openlibrary/app/shelf/ShelvesController.java
+++ app/src/main/java/com/openlibrary/app/shelf/ShelvesController.java
@@ -1,30 +1,22 @@
 package com.openlibrary.app.shelf;
 
-import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping(path = "/users/{userId}/bookshelves")
 public class ShelvesController {
 
-    @GetMapping
-    ResponseEntity<?> list(@PathVariable String userId) {
-        // list all shelves
-        return null;
+    private final ShelvesService shelvesService;
+
+    public ShelvesController(ShelvesService shelvesService) {
+        this.shelvesService = shelvesService;
     }
 
-    @GetMapping("/{shelfId}")
-    ResponseEntity<?> get(@PathVariable String userId, @PathVariable String shelfId) {
-        // get a shelf
-        return null;
-    }
-
-    @GetMapping(path = "/{bookShelfId}/books")
-    ResponseEntity<?> getShelfBooks(@PathVariable String userId, @PathVariable String bookShelfId) {
-        // get books of a shelf
-        return null;
+    @GetMapping("/mylibrary/bookshelves")
+    public List<Shelf> getUserPrivateShelves(Authentication authentication) {
+        return shelvesService.getUserShelves((String) authentication.getPrincipal());
     }
 }
