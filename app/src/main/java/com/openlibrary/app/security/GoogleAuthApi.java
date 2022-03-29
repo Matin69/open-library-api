4bd42c9783021c8915556e85e1b3e761d4bb896e
Matin <matin3enator0@gmail.com>
2022-03-29 11:29:01 +0430

    Add a method to get user info from google apis in GoogleAuthApi

--- app/src/main/java/com/openlibrary/app/security/GoogleAuthApi.java
+++ app/src/main/java/com/openlibrary/app/security/GoogleAuthApi.java
@@ -1,6 +1,10 @@
 package com.openlibrary.app.security;
 
+import com.openlibrary.app.GoogleUserInfoResponse;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -29,6 +33,18 @@ public class GoogleAuthApi {
                 googleAuthProperties.getTokenUrl(),
                 tokenRequest,
                 TokenRequestResponse.class
+        );
+        return response.getBody();
+    }
+
+    public GoogleUserInfoResponse userInfo(String accessToken) {
+        HttpHeaders requestHeaders = new HttpHeaders();
+        requestHeaders.add("Authorization", "Bearer " + accessToken);
+        ResponseEntity<GoogleUserInfoResponse> response = restTemplate.exchange(
+                googleAuthProperties.getUserInfoUrl(),
+                HttpMethod.GET,
+                new HttpEntity<>(requestHeaders),
+                GoogleUserInfoResponse.class
         );
         return response.getBody();
     }
