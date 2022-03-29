29a37fdeb0924642a7f4dc04415fd34e1c29b777
Matin <matin3enator0@gmail.com>
2022-03-29 12:40:02 +0430

    Return current authenticated user info in AuthController

--- app/src/main/java/com/openlibrary/app/security/AuthController.java
+++ app/src/main/java/com/openlibrary/app/security/AuthController.java
@@ -1,5 +1,6 @@
 package com.openlibrary.app.security;
 
+import com.openlibrary.app.user.UserInfo;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,13 +27,15 @@ public class AuthController {
     }
 
     @GetMapping(path = "/oauth2/callback")
-    public ResponseEntity<?> loginCallback(
+    public ResponseEntity<UserInfo> loginCallback(
             @RequestParam(required = false) String code,
             @RequestParam(required = false) String error) {
         if (error != null) {
             throw new IllegalStateException("Google auth error : " + error);
-        } else if (authService.authenticate(code)) {
-            return ResponseEntity.ok().build();
+        }
+        UserInfo authUser = authService.authenticate(code);
+        if (authUser != null) {
+            return ResponseEntity.ok(authUser);
         } else {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
         }
